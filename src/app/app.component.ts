09ecd56b67c486bde104c1440470e69c2f4849d0
basmasalim <basmasalim030@gmail.com>
2025-04-18 16:48:11 +0200

    digital bond

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { HomeComponent } from "./features/pages/home/home.component";
+import { AboutComponent } from "./features/pages/about/about.component";
+import { ProcessComponent } from "./features/pages/process/process.component";
+import { ContactComponent } from "./features/pages/contact/contact.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { ReviewsComponent } from "./features/pages/reviews/reviews.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HomeComponent, AboutComponent, ProcessComponent, ContactComponent, FooterComponent, ReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./core/pages/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
+];
